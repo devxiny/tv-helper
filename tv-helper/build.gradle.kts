8bb7c366ba68e785b7fd8bc4e76ca458ad3cad1e
yanxin <yanxinit@gmail.com>
2025-11-27 15:59:28 +0800

    feat: 支持maven-publish

--- tv-helper/build.gradle.kts
+++ tv-helper/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
@@ -27,20 +28,41 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-    
+
     // Server
     implementation("org.nanohttpd:nanohttpd:2.3.1")
     implementation("org.nanohttpd:nanohttpd-websocket:2.3.1")
-    
+
     // QR Code
     implementation("com.google.zxing:core:3.5.2")
-    
+
     // JSON
     implementation("com.google.code.gson:gson:2.10.1")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+
+                groupId = "com.devxiny.lib"
+                artifactId = "tv-helper"
+                version = "1.0.0"
+            }
+        }
+    }
 }
